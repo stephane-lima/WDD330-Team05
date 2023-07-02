d6baf09bca7a7242a1ac0f8f6bdf9f004225f5d8
stephane-lima <ggabriellopess99999@gmail.com>
2023-07-02 22:04:37 -0100

    Fix JS

--- src/js/externalServices.mjs
+++ src/js/externalServices.mjs
@@ -11,7 +11,7 @@ async function convertToJson(res) {
 }
 
 export async function getProductsByCategory(category) {
-  const response = await fetch(baseURL + `products/search/${category}`);
+  const response = await fetch(baseURL + `/products/search/${category}`);
   const data = await convertToJson(response);
   return data.Result;
 }
